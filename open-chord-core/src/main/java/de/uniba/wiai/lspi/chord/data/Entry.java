dbd1c736ad301b0af815de093afaf0de0738af4e
mh61503891 <mh.on.web@gmail.com>
2015-02-03 11:35:44 +0900

    Refactor

--- open-chord-core/src/main/java/de/uniba/wiai/lspi/chord/data/Entry.java
+++ open-chord-core/src/main/java/de/uniba/wiai/lspi/chord/data/Entry.java
@@ -2,26 +2,18 @@ package de.uniba.wiai.lspi.chord.data;
 
 import java.io.Serializable;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NonNull;
-import lombok.Setter;
-import lombok.ToString;
 
 /**
  * @author Karsten Loesing
  * @author Masayuki Higashino
  * @version 1.0.5
  */
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
-@EqualsAndHashCode
-public final class Entry implements Serializable {
+@Data
+public class Entry implements Serializable {
 
-	private static final long serialVersionUID = 3473253817147038992L;
+	private static final long serialVersionUID = -5143074153184345697L;
 	@NonNull
 	private ID id;
 	@NonNull
